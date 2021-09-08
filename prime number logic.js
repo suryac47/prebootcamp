9a7e0ff5529e10d86a238344cdcf78d307beda9b
suryac47 <90303133+suryac47@users.noreply.github.com>
2021-09-08 17:48:46 +0530

    Add files via upload

---
+++ prime number logic.js
@@ -0,0 +1,17 @@
+var a=8;
+var i=0;
+var flag=0;
+
+for( i=1; i<=a; i++)
+{
+    if(a%i==0)
+    {
+        flag= flag+1;
+    }
+}
+if (flag==2)
+{
+    console.log('True');
+}
+else
+    console.log('Flase');
